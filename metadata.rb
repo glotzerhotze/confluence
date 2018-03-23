dea20cc8058bf7ca859ab6fcb9ff4594c08daca5
Tilo Klessen <t.lo@klessen.cloud>
2018-03-23 17:51:06 +0100

    remove dependencies / bump to version 2.4.5

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 't.lo@klessen.cloud'
 license 'Apache-2.0'
 description 'Installs/Configures Atlassian Confluence'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.4.4'
+version '2.4.5'
 
 issues_url 'https://github.com/glotzerhotze/confluence/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/glotzerhotze/confluence' if respond_to?(:source_url)
@@ -27,9 +27,9 @@ depends 'apache2'
 depends 'ark'
 depends 'database'
 depends 'java'
-depends 'mysql', '< 8.0.0'
-depends 'mysql_connector'
-depends 'mysql2_chef_gem', '< 2.0.0'
+# depends 'mysql', '< 8.0.0'
+# depends 'mysql_connector'
+# depends 'mysql2_chef_gem', '< 2.0.0'
 depends 'postgresql'
 
 chef_version '> 13.0.0' if respond_to?(:chef_version)
